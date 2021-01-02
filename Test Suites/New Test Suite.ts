93cd0e22c17328d543ff8b4f8e01a9f1d236ca46
ngochuynh <ngochuynh@192.168.1.223>
2021-01-02 16:07:40 +0700

    init

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f1c9f70f-0cf9-4364-b0cb-fe8e1e20076b</testSuiteGuid>
+   <testCaseLink>
+      <guid>1aacce7b-230f-4017-8cf8-69e68e51a7d9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Log in test case - record and playback</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
